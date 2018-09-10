6f3929f3edc43ae4636a02a3089256cf9409939e
Rodrigo Ramírez <yitodeveloper@gmail.com>
2018-09-10 12:22:15 -0300

    redux con vanilla js

--- src/entries/redux.js
+++ src/entries/redux.js
@@ -11,6 +11,14 @@ function handleSubmit(event){
     event.preventDefault()
     const data = new FormData($form)
     const title = data.get('title')
+
+    store.dispatch({
+        type: 'ADD_SONG',
+        payload: {
+            title
+        }
+    })
+
     console.log(title)
 }
 
@@ -29,8 +37,37 @@ const initialState = [
     
 ]
 
+const reducer = function(state,action){
+    switch(action.type){
+        case 'ADD_SONG':
+            return [...state,action.payload]
+        default:
+            return state
+    }
+}
+
 const store = createStore(
-    (state) => state,
+    reducer,
     initialState,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
+
+function render(){
+    const $container = document.getElementById('playlist')
+    const playlist = store.getState()
+    $container.innerHTML = ''
+
+    playlist.forEach(item => {
+        const template = document.createElement('p')
+        template.textContent = item.title
+        $container.appendChild(template)
+    });
+}
+
+render()
+
+function handleChange(){
+    render()
+}
+
+store.subscribe(handleChange)

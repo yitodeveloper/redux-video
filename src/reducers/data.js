955311d6d2144bef1a2ef8b560a77db64a894f1f
Rodrigo Ramírez <yitodeveloper@gmail.com>
2018-09-11 08:55:59 -0300

    Renderiza busqueda en el browser

--- src/reducers/data.js
+++ src/reducers/data.js
@@ -4,10 +4,14 @@ function data(state, action){
         case 'SEARCH_VIDEO':{
 
             // action.payload.query
-            const list = state.data.categories[2].playlist
-            const results = list.filter((item)=>{
-                return item.author.includes(action.payload.query)
-            })
+            let results = []
+            if(action.payload.query){
+                const list = state.data.categories[2].playlist
+                results = list.filter((item)=>{
+                    return item.author.includes(action.payload.query)
+                })
+            }
+            
             return {
                 ...state,
                 search: results
